270693c12ec385a4c2003d6e6750cc460053db72
Сергей Деревянченко <sergey.derevyanchenko@yandex.ru>
2022-04-06 09:55:45 +0300

    Sprint 3. Final Iteration.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.select_related('author').all()[:settings.MAX_POSTS]
+    posts = group.posts.select_related('author')[:settings.MAX_POSTS]
     context = {
         'group': group,
         'posts': posts,
